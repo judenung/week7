2a55a0f09ad63a8a545b2c4974b9740840a94cb0
Juden Ung <80509975+judenung@users.noreply.github.com>
2026-09-08 11:20:39 +0800

    Output modified HTML content to console
    Added a command to display the modified content.

--- txtproc.sh
+++ txtproc.sh
@@ -2,3 +2,5 @@
 
 sed -e 's/<b>/<strong>/g; s/<\/b>/<\/strong>/g; s/<i>/<em>/g; s/<\/i>/<\/em>/g; s/kernel/kernal/g; s/http/https/g; s/<p>/\
 <p style="standardtext">/g' inputfile.html > inputfile_u.txt
+
+cat inputfile_u.txt
