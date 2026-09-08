1784d56ac5a9bdb52993609009f9300c508b565b
Juden Ung <80509975+judenung@users.noreply.github.com>
2026-09-08 11:19:00 +0800

    Fix sed command formatting in txtproc.sh

--- txtproc.sh
+++ txtproc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#check
 
-sed -e 's/<b>/<strong>/g; s/<\/b>/<\/strong>/g; s/<i>/<em>/g; s/<\/i>/<\/em>/g; s/kernel/kernal/g; s/http/https/g; s/<p>/\n<p style="standardtext">/g' inputfile.html > inputfile_u.txt
+sed -e 's/<b>/<strong>/g; s/<\/b>/<\/strong>/g; s/<i>/<em>/g; s/<\/i>/<\/em>/g; s/kernel/kernal/g; s/http/https/g; s/<p>/\
+<p style="standardtext">/g' inputfile.html > inputfile_u.txt
