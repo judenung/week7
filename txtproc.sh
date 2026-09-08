cfecb0e5012e94de53fdbe12c90270798ab3b5f5
Juden Ung <80509975+judenung@users.noreply.github.com>
2026-09-08 11:09:03 +0800

    Update txtproc.sh

--- txtproc.sh
+++ txtproc.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
 
+sed -e 's/<b>/<strong>/g; s/</b>/</strong>/g; s/<i>/<em>/g; s/</i>/</em>/g; s/kernel/kernal/g; s/http/https/g; s/<p>/\n<p style="standardtext">' inputfile.html > inputfile_u.txt
